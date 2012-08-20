5c69bcb85985354bd58f9f8df177709d4d84f982
Brianna Laugher <brianna@laugher.id.au>
2012-08-20 16:45:32 +1000

    Add pull request stuff, move more logic into the template and remove use of the reportNoActivity flag for the moment

--- githubactivity.py
+++ githubactivity.py
@@ -25,8 +25,28 @@ class Commit(object):
     def timestamp(self):
         return self._commit.commit.author.date.strftime(dateStamp)
 
+class PullRequest(object):
+    def __init__(self, pull):
+        self._pull = pull
 
-def getRecentCommits(repo, start, end, reportNoActivity):
+    @property
+    def closedTimestamp(self):
+        return self._pull.closed_at.strftime(dateStamp)
+
+    @property
+    def closer(self):
+        return '?'
+
+    @property
+    def author(self):
+        return self._pull.user.login
+
+    @property
+    def title(self):
+        return self._pull.title
+
+
+def getRecentCommits(repo, start):
     """
     @param repo: a GitRepo object
     @return: string
@@ -39,51 +59,40 @@ def getRecentCommits(repo, start, end, reportNoActivity):
         ts = commit.commit.author.date
         if ts < start:
             break
-        if ts < end:
-            # end might be specified, i.e. in the past
-            recentCommits.append(Commit(commit))
-    if not recentCommits and reportNoActivity:
-        return 'No recent commits!'
+        # end might be specified, i.e. in the past
+        recentCommits.append(Commit(commit))
     return recentCommits
 
 
-def getPullRequestsOpen(repo, reportNoActivity):
-    if reportNoActivity:
-        return 'No open pull requests.'
+def getPullRequestsOpen(repo):
+    pulls = repo.get_pulls('open')
+    pulls = [PullRequest(p) for p in pulls]
+    return pulls
+
+
+def getPullRequestsClosed(repo, start):
+    pulls = repo.get_pulls('closed')
+    pulls = [PullRequest(p) for p in pulls if start < p.closed_at]
+    return pulls
+
+
+def getIssuesSubmitted(repo, start):
     return ''
 
 
-def getPullRequestsClosed(repo, reportNoActivity):
-    if reportNoActivity:
-        return 'No closed pull requests.'
+def getIssuesActivity(repo, start):
     return ''
 
 
-def getIssuesSubmitted(repo, reportNoActivity):
-    if reportNoActivity:
-        return 'No issues submitted.'
+def getIssuesClosed(repo, start):
     return ''
 
 
-def getIssuesActivity(repo, reportNoActivity):
-    if reportNoActivity:
-        return 'No activity on issues.'
+def getWikiActivity(repo, start):
     return ''
 
 
-def getIssuesClosed(repo, reportNoActivity):
-    if reportNoActivity:
-        return 'No issues closed.'
-    return ''
-
-
-def getWikiActivity(repo, reportNoActivity):
-    if reportNoActivity:
-        return 'No wiki activity.'
-    return ''
-
-
-def getRepoActivity(org, repo, days=None, end=None, reportNoActivity=None):
+def getRepoActivity(org, repo, days=None, reportNoActivity=None):
     """
     @param org: a string representing an organization
     @param repo: a string representing a repo
@@ -94,23 +103,22 @@ def getRepoActivity(org, repo, days=None, end=None, reportNoActivity=None):
     """
     if not days:
         days = 7
-    if not end:
-        end = datetime.datetime.today()
     if reportNoActivity is None:
         reportNoActivity = True
+    end = datetime.datetime.today()
     period = datetime.timedelta(days=days)
     start = end - period
 
     g = Github()
     repository = g.get_organization(org).get_repo(repo)
     
-    commits = getRecentCommits(repository, start, end, reportNoActivity)
-    pullReqOpen = getPullRequestsOpen(repository, reportNoActivity)
-    pullReqClosed = getPullRequestsClosed(repository, reportNoActivity)
-    issuesSubmitted = getIssuesSubmitted(repository, reportNoActivity)
-    issuesActivity = getIssuesActivity(repository, reportNoActivity)
-    issuesClosed = getIssuesClosed(repository, reportNoActivity)
-    wiki = getWikiActivity(repository, reportNoActivity)
+    commits = getRecentCommits(repository, start)
+    pullReqOpen = getPullRequestsOpen(repository)
+    pullReqClosed = getPullRequestsClosed(repository, start)
+    issuesSubmitted = getIssuesSubmitted(repository, start)
+    issuesActivity = getIssuesActivity(repository, start)
+    issuesClosed = getIssuesClosed(repository, start)
+    wiki = getWikiActivity(repository, start)
 
     template = Template(filename='template.txt')
     contents = {
@@ -143,7 +151,7 @@ if __name__ ==  '__main__':
     parser.add_argument("-o", dest="org", required=True, help="Name of an organization")
     parser.add_argument("-r", dest="repo", required=True, help="Name of a repository")
     parser.add_argument("-d", dest="days", default=None, type=int, help="Number of days to summarise")
-    parser.add_argument("-e", dest="end", default=None, type=dateObject, help="End date (in format '{}'".format(dateStamp))
+#    parser.add_argument("-e", dest="end", default=None, type=dateObject, help="End date (in format '{}'".format(dateStamp))
     parser.add_argument("-n", dest="reportNoActivity", default=None, type=bool, help="Report explicitly if there is no activity")
     args = parser.parse_args()
     r = getRepoActivity(**vars(args))
